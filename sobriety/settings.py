c3fff1e78e512260e38d8923bd4c642d6856446e
TECH <wendyachieng98@gmail.com>
2026-03-12 13:54:27 +0300

    add database_url

--- sobriety/settings.py
+++ sobriety/settings.py
@@ -85,6 +85,14 @@ DATABASES = {
     }
 }
 
+DATABASE_URL = os.getenv('DATABASE_URL')
+if DATABASE_URL:
+    DATABASES['default'] = dj_database_url.config(
+        default=DATABASE_URL,
+        conn_max_age=600,
+        conn_health_checks=True,
+    )
+
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
